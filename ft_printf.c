fd16da45dd7ba09e2bc0e19b38cfe377af102c06
Jorge luis Simelio jurado <jsimelio@f1r1s15.codam.nl>
2020-12-07 19:12:11 +0100

    201207 EOD

--- ft_printf.c
+++ ft_printf.c
@@ -6,24 +6,29 @@
 /*   By: jsimelio <jsimelio@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/30 16:57:34 by jsimelio      #+#    #+#                 */
-/*   Updated: 2020/12/01 19:47:41 by jsimelio      ########   odam.nl         */
+/*   Updated: 2020/12/07 18:56:38 by jsimelio      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-#include "ft_putchar_fd.c"
-#include "ft_putnbr_fd.c"
-#include "ft_putstr_fd.c"
+#include "./libft/ft_putchar_fd.c"
+#include "./libft/ft_putint_fd.c"
+#include "./libft/ft_putstr_fd.c"
+#include "./libft/ft_putuint_fd.c"
+#include "./libft/ft_putint_base_fd.c"
+#include "./libft/ft_strlen.c"
 
 int			main(void)
 {
-	int		num = -300;
+	int		num = -3;
+	int		hex = 16;
 	// int		num2 = 500;
 	char	*str = "Hello";
 	// char	*str2 = "Bye";
 	char 	c = '9';
 
-	ft_printf("int: %d, string: %s, char: %c, unsigned int: %u", num, str, c, num);
+	ft_printf("ft_printf:\nint: %d, string: %s, char: %c, unsigned int: %u, hex: %x\n", num, str, c, num, hex);
+	printf("printf:\nint: %d, string: %s, char: %c, unsigned int: %u, hex: %x\n", num, str, c, num, hex);
 	return (0);
 }
 
@@ -32,16 +37,16 @@ static void	ft_parse_int(va_list *ap)
 		int	n;
 
 		n = va_arg (*ap, int);
-		ft_putnbr_fd(n, 1);
+		ft_putint_fd(n, 1);
 }
 
-// This is still printing as a negative... why?
-static void	ft_parse_uns(va_list *ap)
+// This is still printing as a negative sign... why?
+static void	ft_parse_uint(va_list *ap)
 {
 		unsigned int	n;
 
-		n = va_arg (*ap, int);
-		ft_putnbr_fd(n, 1);
+		n = va_arg (*ap, unsigned int);
+		ft_putuint_fd(n, 1);
 }
 
 static void	ft_parse_str(va_list *ap)
@@ -60,6 +65,16 @@ static void	ft_parse_char(va_list *ap)
 		ft_putchar_fd(c, 1);
 }
 
+static void	ft_parse_hexlow(va_list *ap)
+{
+		char	*base;
+		int		c;
+
+		base = "0123456789abcdef";
+		c = va_arg (*ap, int);
+		ft_putint_base_fd(c, base, 1);
+}
+
 static void	ft_parse(const char **str, va_list *ap)
 {
 	(*str)++;
@@ -70,7 +85,9 @@ static void	ft_parse(const char **str, va_list *ap)
 	else if (**str == 'c')
 		ft_parse_char(ap);
 	else if (**str == 'u')
-		ft_parse_uns(ap);
+		ft_parse_uint(ap);
+	else if (**str == 'x')
+		ft_parse_hexlow(ap);
 	else if (**str == '%')
 		ft_putchar_fd('%', 1);
 	(*str)++;
--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: jsimelio <jsimelio@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/30 17:00:11 by jsimelio      #+#    #+#                 */
-/*   Updated: 2020/12/01 19:01:42 by jsimelio      ########   odam.nl         */
+/*   Updated: 2020/12/07 18:15:18 by jsimelio      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,4 @@
 # include <stdio.h>
 
 int		ft_printf(const char *str, ...);
-int		ft_putchar_fd(char c, int fd);
-void	ft_putnbr_fd(int n, int fd);
-void	ft_putstr_fd(char *s, int fd);
 #endif
